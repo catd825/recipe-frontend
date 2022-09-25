1b5e6540e2fed0b9edf66c6e7334813d3d82b0d1
catd825 <60204063+catd825@users.noreply.github.com>
2022-09-25 17:54:08 -0400

    added material ui, flexgrid

--- src/Components/RecipeCard.tsx
+++ src/Components/RecipeCard.tsx
@@ -1,11 +1,12 @@
 import { NavLink } from "react-router-dom";
 import { IRecipe } from "../interfaces";
+import Grid from "@mui/material/Grid";
 
 export const RecipeCard = (recipe: IRecipe) => {
   return (
-    <div>
-      <img alt="recipe_img" src={recipe.img_url} />
+    <Grid item xs={2} sm={4} md={4} style={{ maxWidth: "360px", margin: 20 }}>
+      <img alt="recipe_img" src={recipe.img_url} width="350" height="250" />
       <NavLink to={`/recipes/${recipe.id}`}>{recipe.title}</NavLink>
-    </div>
+    </Grid>
   );
 };
--- src/Components/RecipeList.tsx
+++ src/Components/RecipeList.tsx
@@ -1,5 +1,7 @@
 import { IRecipe } from "../interfaces";
 import { RecipeCard } from "./RecipeCard";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
 
 export interface IProps {
   recipes: IRecipe[];
@@ -8,7 +10,13 @@ export interface IProps {
 export const RecipeList = (props: IProps) => {
   const { recipes } = props;
   const mapRecipes = recipes.map((recipe: IRecipe) => {
-    return <RecipeCard {...recipe} key={recipe.id}/>;
+    return (
+      <Box sx={{ display: "inline-flex" }}>
+        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+          <RecipeCard {...recipe} key={recipe.id} />
+        </Grid>
+      </Box>
+    );
   });
 
   return <>{mapRecipes}</>;
